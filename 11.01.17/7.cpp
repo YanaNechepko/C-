67f5a9308f47c626601b153648e07c404ae5a597
Yana <yana.nechepko@mail.ru>
2017-01-18 21:20:24 +0300

    program

--- 11.01.17/7.cpp
+++ 11.01.17/7.cpp
@@ -1,61 +1,61 @@
+
 #include <iostream>
 using namespace std;
-int search(int a, int b);
-struct ABC {
-    int x;
-    int y;
-};
-ABC s[101] = {};
-int main()
-{
-    int n = 0;    //îñü X
-    int m = 0;    //îñü Y
-    cin >> n;
-    cin >> m;
-    int k = 0;
-    cin >> k;
 
-    for(int i = 0; i < k; i++)        //ââîäèì êîîğäèíàòû ïğîõîäíûõ êâàğòàëîâ
-    {
-        cin >> s[i].x;
-        cin >> s[i].y;
-    }
-    int **l = new int* [n+1];
-    for (int count = 0; count < n+1; count++)
-        l[count] = new int [m+1];
-
-    for(int i = 0; i <= n; i++)
-    {
-        for(int j = 0; j <= m; j++)
-        {
-            l[i][j] = 0;        //ïğîñòî çàïîëíÿåì ìàññèâ íóëÿìè
-        }
-    }
-    int b = 0;
-    for(int i = 1; i <= n; i++)
-    {
-        for(int j = 1; j <= m; j++)        //èäåì ñíèçó ââåğõ è ñëåâà íàïğàâî
-        {
-            l[i][j] = l[i-1][j];
-            if (b == 1) { l[i][j] = l[i][j-1]; }
-            if (search(i, j) && l[i-1][j] == l[i][j-1]) { l[i][j] += 1; b = 1; }
-            if (l[i][j] < l[i-1][j]) l[i][j] = l[i-1][j];
-        }
-        b = 0;
-    }
-    long long res = 0;
-    res = (m + n) * 100 - l[n][m] * 58.5;    // â l[n][m] õğàíèòñÿ êîëè÷åñòâî ïğîõîäíûõ êâàğòàëîâ
-    cout << res;
-    for (int count = 0; count < n+1; count++) delete [] l[count];
-    system("pause");
-    return 0;
+double min(double a, double b) {
+	if (a>=b)
+	 {
+		return b;
+	 }
+		return a;
 }
-int search(int a, int b)    //ôóíêöèÿ ïîèñêà ïğîõîäíûõ êâàğòàëîâ
-{
-    for(int i = 0;; i++)
-    {
-        if (s[i].x == a && s[i].y == b) return 1;
-        if (s[i].x == 0 && s[i].y == 0) break; //áàğüåğ
-    }
-    return 0;
+
+double dlinaputi(int i, int j, double **prkst, bool **etoskver) {
+	if (prkst[i][j]!=-1)
+	 {
+		return prkst[i][j];
+	 }
+	if (etoskver[i][j])
+     {
+		prkst[i][j] = min(min(dlinaputi(i - 1, j, prkst, etoskver), dlinaputi(i, j - 1, prkst, etoskver)) + 100, dlinaputi(i - 1, j - 1, prkst, etoskver) + 141.4213);
+		return prkst[i][j];
+	 }
+	prkst[i][j] = min(dlinaputi(i - 1, j, prkst, etoskver), dlinaputi(i, j - 1, prkst, etoskver)) + 100;
+	return prkst[i][j];
+}
+
+void shortest_path(int n, int m) {
+	double **prkst = new double*[n + 1];
+	bool **etoskver = new bool*[n + 1];
+	for (int count = 0; count <= n; count++)
+	 {
+		etoskver[count] = new bool[m + 1];
+		prkst[count] = new double[m + 1];
+	 }
+	for (int i = 0; i < m + 1; i++)
+	 {
+		for (int j = 0; j < n + 1; j++)
+		 {
+			etoskver[j][i] = false;
+			prkst[j][i] = -1;
+			prkst[j][0] = 100 * j;
+		 }
+		prkst[0][i] = 100 * i;
+	 }
+	int kolvoparkov;
+	cin>>kolvoparkov;
+	for (int i = 0; i < kolvoparkov; i++)
+	 {
+		int n_i, m_i;
+		cin>>n_i>> m_i;
+		etoskver[n_i][m_i] = true;
+	 }
+	cout<<(int)(dlinaputi(n, m, prkst, etoskver) + 0.5);
+}
+
+int main() {
+	int n, m;
+	cin>>n>>m;
+	shortest_path(n, m);
+	return 0;
 }
